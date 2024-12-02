b49ab5fef9b60ea68b30d4c7936bd1d76ae41e53
Gio Andrew Briones <gioandrewbriones@gmail.com>
2024-12-02 21:38:59 +0800

    Add additional test messages to App.java

--- src/App.java
+++ src/App.java
@@ -3,6 +3,9 @@ public class App {
         System.out.println("Welcome to book inventory management");
 
         System.out.println("Testing q lang ehe");
+        System.out.println("Testing q lang ulit anoiba");
+        System.out.println("Testing q lang promise");
+        
 
     }
 }
