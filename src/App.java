75abae925ac54ecf7ca7d0314d3ab5fe9ea15599
Gio Andrew Briones <gioandrewbriones@gmail.com>
2024-12-02 21:32:30 +0800

    Test Commit

--- src/App.java
+++ src/App.java
@@ -2,5 +2,7 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Welcome to book inventory management");
 
+        System.out.println("Testing q lang ehe");
+
     }
 }
